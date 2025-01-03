bc08f075ddb19c4f246e66910d130df4bd8165f2
shada <103006276+shadaESQ@users.noreply.github.com>
2025-01-03 20:57:38 +0300

    Dark mood & nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
-import { SignedOut,SignInButton,SignedIn,UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ui/modeToggle";
 
 export default function Home() {
   return (
-    <div>
-    <SignedOut>
-        <SignInButton mode="modal" />
-        <button className="bg-red-500">
-          sign in
-        </button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle/>
+      <Button variant={"secondary"}>Click Me</Button>
+
     </div>
   );
 }
